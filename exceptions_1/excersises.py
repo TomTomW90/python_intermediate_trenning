91251903512d9d2cf75eda60abd1c6d5b647c432
tomasz-wiaderek <t.wiaderek@gmail.com>
2021-02-07 10:12:08 +0100

    Exception_1 updated

--- exceptions_1/excersises.py
+++ exceptions_1/excersises.py
@@ -1,5 +1,20 @@
 def case_1():
+    # given
     list_of_numbers = [1, 5, 8, 10, 21]
+    # when
     print('Case_1 before')
-    result = list_of_numbers[5]
+    # then; wersja pierwsza
+    try:
+        result = list_of_numbers[5]
+    except IndexError as ie:
+        print(f'Exception couht {ie.args}')
+    except Exception as exp:
+        print(f'Exception cought {exp.args}')
+
     print('Case_1 after')
+
+    # then; wersja druga
+    try:
+        result = list_of_numbers[5]
+    except (IndexError, Exception) as e:
+        print(f'Exception cought by touple {e.args}')
