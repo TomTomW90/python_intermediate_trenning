25abf174d8b28c5e8f67b8d61d78e370c9055455
tomasz-wiaderek <t.wiaderek@gmail.com>
2021-02-21 16:45:23 +0100

    Lambda Case_1 added.

--- lambda_cases/case_1.py
+++ lambda_cases/case_1.py
@@ -1,5 +1,12 @@
 def lamb_case_1():
-    list_of_names = ['Ala', 'Jola', 'Jan', 'Marek', 'Marzena']
-    print('Case1a')
-
-    sorted(list_of_names, key=lambda name: name)
+    list_of_names = ['Ala', 'Jola', 'Marzena', 'Jan', 'Marek']
+    print('Case_1A')
+    a_list = sorted(list_of_names, key=lambda name: len(name))
+    print(list_of_names)
+    print(a_list)
+    print('\nCase_1B')
+    b_list = sorted(list_of_names, key=lambda name: len(name), reverse=True)
+    print(b_list)
+    print('\nCase_1C')
+    c_list = sorted(list_of_names)
+    print(c_list)
